818010d69b1d5503699f0e5738752208c1e1b9fb
xyh-cosmo <yhxu@nao.cas.cn>
2021-02-05 16:40:12 +0800

    update CCD231-debug-2021-01-26.srcs

--- VLog/01_spi/CCD231-debug-2021-01-26.srcs/constrs_1/imports/new/top.xdc
+++ VLog/01_spi/CCD231-debug-2021-01-26.srcs/constrs_1/imports/new/top.xdc
@@ -19,16 +19,16 @@ set_property PACKAGE_PIN AD18 [get_ports A1]
 #########################################################
 #==== 1MHz的方波 ====#
 # RST.SIG-CTR-1-2
-#set_property PACKAGE_PIN AE18 [get_ports sq_wave]
+set_property PACKAGE_PIN AE18 [get_ports RST_SIG_CTR]
 
 #==== RPhi1-CTR-1-2
-#set_property PACKAGE_PIN AF18 [get_ports sq_wave]
+set_property PACKAGE_PIN AF18 [get_ports RPHI1_CTR]
 
 # RPhi2-CTR-1-2
-set_property PACKAGE_PIN AB25 [get_ports sq_wave]
+set_property PACKAGE_PIN AB25 [get_ports RPHI2_CTR]
 
 #==== RPhi3-CTR-1-2
-#set_property PACKAGE_PIN AA25 [get_ports sq_wave]
+set_property PACKAGE_PIN AA25 [get_ports RPHI3_CTR]
 #########################################################
 
 set_property IOSTANDARD LVCMOS18 [get_ports mosi]
@@ -36,69 +36,9 @@ set_property IOSTANDARD LVCMOS18 [get_ports sclk]
 #set_property IOSTANDARD LVCMOS18 [get_ports cs]
 set_property IOSTANDARD LVCMOS18 [get_ports A0]
 set_property IOSTANDARD LVCMOS18 [get_ports A1]
-set_property IOSTANDARD LVCMOS18 [get_ports sq_wave]
+set_property IOSTANDARD LVCMOS18 [get_ports RST_SIG_CTR]
+set_property IOSTANDARD LVCMOS18 [get_ports RPHI1_CTR]
+set_property IOSTANDARD LVCMOS18 [get_ports RPHI2_CTR]
+set_property IOSTANDARD LVCMOS18 [get_ports RPHI3_CTR]
 
 #create_clock -period 5.000 -name sys_clk -waveform {0.000 10.000} [get_ports sys_clk]
-
-
-
-
-
-create_debug_core u_ila_0 ila
-set_property ALL_PROBE_SAME_MU true [get_debug_cores u_ila_0]
-set_property ALL_PROBE_SAME_MU_CNT 1 [get_debug_cores u_ila_0]
-set_property C_ADV_TRIGGER false [get_debug_cores u_ila_0]
-set_property C_DATA_DEPTH 16384 [get_debug_cores u_ila_0]
-set_property C_EN_STRG_QUAL false [get_debug_cores u_ila_0]
-set_property C_INPUT_PIPE_STAGES 0 [get_debug_cores u_ila_0]
-set_property C_TRIGIN_EN false [get_debug_cores u_ila_0]
-set_property C_TRIGOUT_EN false [get_debug_cores u_ila_0]
-set_property port_width 1 [get_debug_ports u_ila_0/clk]
-connect_debug_port u_ila_0/clk [get_nets [list my_clock/inst/clk_150M]]
-set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe0]
-set_property port_width 1 [get_debug_ports u_ila_0/probe0]
-connect_debug_port u_ila_0/probe0 [get_nets [list spi_control/A0]]
-create_debug_port u_ila_0 probe
-set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe1]
-set_property port_width 1 [get_debug_ports u_ila_0/probe1]
-connect_debug_port u_ila_0/probe1 [get_nets [list clk_5M]]
-create_debug_port u_ila_0 probe
-set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe2]
-set_property port_width 1 [get_debug_ports u_ila_0/probe2]
-connect_debug_port u_ila_0/probe2 [get_nets [list clk_10M]]
-create_debug_port u_ila_0 probe
-set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe3]
-set_property port_width 1 [get_debug_ports u_ila_0/probe3]
-connect_debug_port u_ila_0/probe3 [get_nets [list clk_25M]]
-create_debug_port u_ila_0 probe
-set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe4]
-set_property port_width 1 [get_debug_ports u_ila_0/probe4]
-connect_debug_port u_ila_0/probe4 [get_nets [list clk_150M]]
-create_debug_port u_ila_0 probe
-set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe5]
-set_property port_width 1 [get_debug_ports u_ila_0/probe5]
-connect_debug_port u_ila_0/probe5 [get_nets [list clk_div/clk_div]]
-create_debug_port u_ila_0 probe
-set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe6]
-set_property port_width 1 [get_debug_ports u_ila_0/probe6]
-connect_debug_port u_ila_0/probe6 [get_nets [list spi_control/cs]]
-create_debug_port u_ila_0 probe
-set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe7]
-set_property port_width 1 [get_debug_ports u_ila_0/probe7]
-connect_debug_port u_ila_0/probe7 [get_nets [list spi_control/mosi]]
-create_debug_port u_ila_0 probe
-set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe8]
-set_property port_width 1 [get_debug_ports u_ila_0/probe8]
-connect_debug_port u_ila_0/probe8 [get_nets [list rst_w]]
-create_debug_port u_ila_0 probe
-set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe9]
-set_property port_width 1 [get_debug_ports u_ila_0/probe9]
-connect_debug_port u_ila_0/probe9 [get_nets [list spi_control/sclk]]
-create_debug_port u_ila_0 probe
-set_property PROBE_TYPE DATA_AND_TRIGGER [get_debug_ports u_ila_0/probe10]
-set_property port_width 1 [get_debug_ports u_ila_0/probe10]
-connect_debug_port u_ila_0/probe10 [get_nets [list spi_control/status]]
-set_property C_CLK_INPUT_FREQ_HZ 300000000 [get_debug_cores dbg_hub]
-set_property C_ENABLE_CLK_DIVIDER false [get_debug_cores dbg_hub]
-set_property C_USER_SCAN_CHAIN 1 [get_debug_cores dbg_hub]
-connect_debug_port dbg_hub/clk [get_nets clk_150M]
--- VLog/01_spi/CCD231-debug-2021-01-26.srcs/sources_1/new/top.v
+++ VLog/01_spi/CCD231-debug-2021-01-26.srcs/sources_1/new/top.v
@@ -9,23 +9,168 @@
 //	   
 
 module TOP(
-	input clk_sys,
-	// input rst_sys,
+//  与PS交互部分
+    DDR_addr,
+    DDR_ba,
+    DDR_cas_n,
+    DDR_ck_n,
+    DDR_ck_p,
+    DDR_cke,
+    DDR_cs_n,
+    DDR_dm,
+    DDR_dq,
+    DDR_dqs_n,
+    DDR_dqs_p,
+    DDR_odt,
+    DDR_ras_n,
+    DDR_reset_n,
+    DDR_we_n,
 
-	//	SPI outputs
-	output sclk,
-	output mosi,
-	output A0,
-	output A1,
-//	output cs,
+    FIXED_IO_ddr_vrn,
+    FIXED_IO_ddr_vrp,
+    FIXED_IO_mio,
+    FIXED_IO_ps_clk,
+    FIXED_IO_ps_porb,
+    FIXED_IO_ps_srstb,
+//    gpio_tri_o,
+    
+//  =================
+	clk_sys,
+	// rst_sys,
 
-	//  square waves, which can also be viewed as a clock signal
-	output sq_wave,
+//	SPI outputs
+	sclk,
+	mosi,
+	A0,
+	A1,
+//	cs,
+
+    RST_SIG_CTR,
+    RPHI1_CTR,
+    RPHI2_CTR,
+    RPHI3_CTR,
+    
+//  LEDs
+    leds,
 	
-	input PL_KEY
+	PL_KEY
 	);
 
-//  clocks
+  output [3:0] leds;
+  wire [3:0] leds;
+
+// ==========================================================================
+// ==========================================================================
+//  与PS交互部分
+  inout [14:0]DDR_addr;
+  inout [2:0]DDR_ba;
+  inout DDR_cas_n;
+  inout DDR_ck_n;
+  inout DDR_ck_p;
+  inout DDR_cke;
+  inout DDR_cs_n;
+  inout [3:0]DDR_dm;
+  inout [31:0]DDR_dq;
+  inout [3:0]DDR_dqs_n;
+  inout [3:0]DDR_dqs_p;
+  inout DDR_odt;
+  inout DDR_ras_n;
+  inout DDR_reset_n;
+  inout DDR_we_n;
+//  output FCLK_CLK0;
+  inout FIXED_IO_ddr_vrn;
+  inout FIXED_IO_ddr_vrp;
+  inout [53:0]FIXED_IO_mio;
+  inout FIXED_IO_ps_clk;
+  inout FIXED_IO_ps_porb;
+  inout FIXED_IO_ps_srstb;
+  
+//  input [0:0]gpio_in_tri_i;
+//  output [31:0]gpio2_tri_o;
+//  output [3:0]gpio_tri_o;
+
+  wire [14:0]DDR_addr;
+  wire [2:0]DDR_ba;
+  wire DDR_cas_n;
+  wire DDR_ck_n;
+  wire DDR_ck_p;
+  wire DDR_cke;
+  wire DDR_cs_n;
+  wire [3:0]DDR_dm;
+  wire [31:0]DDR_dq;
+  wire [3:0]DDR_dqs_n;
+  wire [3:0]DDR_dqs_p;
+  wire DDR_odt;
+  wire DDR_ras_n;
+  wire DDR_reset_n;
+  wire DDR_we_n;
+//  wire FCLK_CLK0;
+  wire FIXED_IO_ddr_vrn;
+  wire FIXED_IO_ddr_vrp;
+  wire [53:0]FIXED_IO_mio;
+  wire FIXED_IO_ps_clk;
+  wire FIXED_IO_ps_porb;
+  wire FIXED_IO_ps_srstb;
+  wire [31:0] gpio2_tri_o;
+//  wire [0:0] gpio_in_tri_i;
+  wire [3:0] gpio_tri_o;
+
+  wire fclk_clk0;
+  
+  reg pl_status = 1'b0;
+  
+
+  
+  CCD231_wrapper CCD231
+       (.DDR_addr(DDR_addr),
+        .DDR_ba(DDR_ba),
+        .DDR_cas_n(DDR_cas_n),
+        .DDR_ck_n(DDR_ck_n),
+        .DDR_ck_p(DDR_ck_p),
+        .DDR_cke(DDR_cke),
+        .DDR_cs_n(DDR_cs_n),
+        .DDR_dm(DDR_dm),
+        .DDR_dq(DDR_dq),
+        .DDR_dqs_n(DDR_dqs_n),
+        .DDR_dqs_p(DDR_dqs_p),
+        .DDR_odt(DDR_odt),
+        .DDR_ras_n(DDR_ras_n),
+        .DDR_reset_n(DDR_reset_n),
+        .DDR_we_n(DDR_we_n),
+        .FCLK_CLK0(fclk_clk0),
+        .FIXED_IO_ddr_vrn(FIXED_IO_ddr_vrn),
+        .FIXED_IO_ddr_vrp(FIXED_IO_ddr_vrp),
+        .FIXED_IO_mio(FIXED_IO_mio),
+        .FIXED_IO_ps_clk(FIXED_IO_ps_clk),
+        .FIXED_IO_ps_porb(FIXED_IO_ps_porb),
+        .FIXED_IO_ps_srstb(FIXED_IO_ps_srstb),
+        .gpio_in_tri_i(pl_status),
+        .gpio2_tri_o(gpio2_tri_o),
+        .gpio_tri_o(gpio_tri_o)
+        );
+
+// ==========================================================================
+// ==========================================================================
+
+// ==========================================================================
+    input clk_sys;
+//	SPI outputs
+	output sclk;
+	output mosi;
+	output A0;
+	output A1;
+
+    output RST_SIG_CTR;
+    output RPHI1_CTR;
+    output RPHI2_CTR;
+    output RPHI3_CTR;
+    
+
+	
+	input PL_KEY;
+// ==========================================================================
+
+//  生成50MHz的时钟（也包括其他频率的时钟） 
     (*mark_debug="true"*) wire clk_150M;
     (*mark_debug="true"*) wire clk_50M;
     (*mark_debug="true"*) wire clk_25M;
@@ -46,20 +191,111 @@ module TOP(
         .clk_in1(clk_sys)
         );
 
-//	鐢≒L鎸夐敭鐨勮Е鍙戞潵妯℃嫙reset
+
+	//	states of the STATE MACHINE
+	reg[3:0] state;
+	parameter S_IDLE	= 4'd0;
+	parameter S_SPI	= 4'd1;
+	parameter S_CTR	= 4'd2;
+
+	reg en_spi = 1'b0;
+	reg en_ctr = 1'b0;
+	reg spi_done = 1'b0;
+	reg ctr_done  = 1'b0;
+
+	wire spi_status;
+	wire ctr_status;
+	reg rst_w;
+
+    wire cs;    // not used actually ...
+	SPI_CONTROL spi_control(
+		.clk(clk_50M),
+		.rst(rst_w),
+		.en(en_spi),
+		.sclk(sclk),
+		.mosi(mosi),
+		.cs(cs),
+		.A0(A0),
+		.A1(A1),
+		.status(spi_status)
+		);
+
+//  分频时钟
+//    parameter cycles_max = 20;
+    parameter cycles_max = 0;  // set to zero so that the divided clocks runs forever!
+	CLOCK_DIV
+		#(
+			.DIV_FACTOR(50),
+			.CNT_START(0),
+			.CYCLES_MAX(cycles_max)
+		)
+		clk_div_0
+		(
+			.clk_sys(clk_50M),
+			.en(en_ctr),
+			.clk_div(RST_SIG_CTR),
+			.status(ctr_status)
+		);
+		
+	CLOCK_DIV
+        #(
+            .DIV_FACTOR(50),
+            .CNT_START(0),
+            .CYCLES_MAX(cycles_max)
+        )
+        clk_div_1
+        (
+            .clk_sys(clk_50M),
+            .en(en_ctr),
+            .clk_div(RPHI1_CTR),
+            .status()
+        );
+
+    CLOCK_DIV
+		#(
+			.DIV_FACTOR(50),
+			.CNT_START(0),
+			.CYCLES_MAX(cycles_max)
+		)
+		clk_div_2
+		(
+			.clk_sys(clk_50M),
+			.en(en_ctr),
+			.clk_div(RPHI2_CTR),
+			.status()
+		);
+
+    CLOCK_DIV
+		#(
+			.DIV_FACTOR(50),
+			.CNT_START(0),
+			.CYCLES_MAX(cycles_max)
+		)
+		clk_div_3
+		(
+			.clk_sys(clk_50M),
+			.en(en_ctr),
+			.clk_div(RPHI3_CTR),
+			.status()
+		);
+// =============================
+	
+//	通过PL按键来触发一个复位reset
     reg state_pl_key        = 1'b0;
     reg state_pl_key_bak    = 1'b0;      
     always@( negedge PL_KEY ) begin
         state_pl_key <= ~state_pl_key;
     end
     
-    (*mark_debug="true"*)reg rst_w;
     reg [31:0] rst_cn = 32'd0;
     localparam rst_start = 32'd1000000;
-
-
+    reg[3:0] leds_r = 4'd0;
+    assign leds = leds_r;
+    
     always@(posedge clk_sys) begin
         if( state_pl_key != state_pl_key_bak ) begin
+            pl_status <= ~pl_status;
+            
             if( rst_cn < rst_start ) begin
                 rst_w <= 1'b0;
                 rst_cn <= rst_cn + 32'd1;
@@ -76,54 +312,13 @@ module TOP(
         end
     end
 
-	//	states of the STATE MACHINE
-	reg[3:0] state;
-	parameter S_IDLE	= 4'd0;
-	parameter S_SPI	= 4'd1;
-	parameter S_SQ		= 4'd2;
-
-	reg en_spi = 1'b0;
-	reg en_sq  = 1'b0;
-	reg spi_done = 1'b0;
-	reg sq_done  = 1'b0;
-
-	wire spi_status;
-	wire sq_status;
-
-	SPI_CONTROL spi_control(
-		.clk(clk_50M),
-		.rst(rst_w),
-		.en(en_spi),
-		.sclk(sclk),
-		.mosi(mosi),
-		.cs(cs),
-		.A0(A0),
-		.A1(A1),
-		.status(spi_status)
-		);
-
-	CLOCK_DIV
-		#(
-			.DIV_FACTOR(50),
-			.CNT_START(0),
-			// .CYCLES_MAX(20)
-			.CYCLES_MAX(0)
-		)
-		clk_div
-		(
-			.clk_sys(clk_50M),
-			.en(en_sq),
-			.clk_div(sq_wave),
-			.status(sq_status)
-		);
-
 	always@( posedge clk_sys or posedge rst_w ) begin
 		if( rst_w ) begin
 			state	<= S_IDLE;
 			en_spi	<= 1'b0;
-			en_sq	<= 1'b0;
-			spi_done<= 1'b0;
-			sq_done <= 1'b0;
+			en_ctr  <= 1'b0;
+			spi_done <= 1'b0;
+			ctr_done <= 1'b0;
 		end
 		else begin
 			case( state )
@@ -131,6 +326,7 @@ module TOP(
 				begin
 					if( spi_status == 1'b0 && spi_done == 1'b0 ) begin
 						en_spi	<= 1'b1;
+						en_ctr  <= 1'b0;
 						state	<= S_SPI;
 					end
 				end
@@ -141,19 +337,19 @@ module TOP(
 						en_spi	<= 1'b0;
 						spi_done <= 1'b1;
 
-						if( sq_status == 1'b0 && sq_done == 1'b0 ) begin
-							en_sq   <= 1'b1;
-							state	<= S_SQ;
+						if( ctr_status == 1'b0 && ctr_done == 1'b0 ) begin
+							en_ctr  <= 1'b1;
+							state	<= S_CTR;
 						end
 					end
 				end
 
-				S_SQ:
+				S_CTR:
 				begin
-					if( sq_status == 1'b1 ) begin
-						en_sq	<= 1'b0;
-						sq_done <= 1'b1;
-						state	<= S_IDLE;
+					if( ctr_status == 1'b1 ) begin
+						en_ctr	 <= 1'b0;
+						ctr_done <= 1'b1;
+						state	 <= S_IDLE;
 					end
 				end
 
@@ -162,5 +358,28 @@ module TOP(
 			endcase
 		end
 	end
+
+// 利用LED灯进行测试：
+    reg[31:0] cnt = 32'd0;
+    wire ps_clk;
+    
+//    assign ps_clk = gpio_tri_o[3];
+    assign ps_clk = gpio2_tri_o[3];
+    
+    assign leds = leds_r;
+       
+    always@( posedge ps_clk ) begin
+//        if( cnt >= 32'd9999 ) begin
+        if( cnt >= 32'd999_999 ) begin
+            leds_r <= ~leds_r;
+            cnt <= 32'd0;
+//            pl_status <= ~pl_status;
+        end
+        else
+            cnt <= cnt + 32'd1;
+    end
+    
+
+// 添加逻辑分析仪
 
 endmodule
